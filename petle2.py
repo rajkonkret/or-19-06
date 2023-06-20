59279cf07a6756dc066ead57a16ed19424ef70da
CSComarch <rajkonkret660@gmail.com>
2023-06-20 10:49:20 +0200

    add petle2.py

---
+++ petle2.py
@@ -0,0 +1,37 @@
+dictionary = {'imie': 'Radek', 'nazwisko': 'Kowalski'}
+
+print(dictionary)
+print(type(dictionary))  # <class 'dict'>
+
+# wyswietla klucze
+for k in dictionary:
+    print(k)
+
+for k in dictionary.keys():
+    print(k)
+
+print(dictionary.keys())
+
+# wypisze wartości
+for v in dictionary.values():
+    print(v)
+
+print(dictionary.items())  # dict_items([('imie', 'Radek'), ('nazwisko', 'Kowalski')])
+
+for i in dictionary.items():
+    print(i)  # ('nazwisko', 'Kowalski')
+
+for k, v in dictionary.items():
+    print(k, "=>", v)  # nazwisko => Kowalski
+
+dictionary2 = {'name': 'imie', 'company': 'Orange'}
+print(dictionary2)
+# {'name': 'imie', 'company': 'Orange'}
+print({value: key for key, value in dictionary2.items()})
+# {'imie': 'name', 'Orange': 'company'}
+
+d2 = {}
+for k, v in dictionary2.items():
+    d2[v] = k # zamiana miejscami klucza z wartoscią
+print(d2)
+# {'imie': 'name', 'Orange': 'company'}
