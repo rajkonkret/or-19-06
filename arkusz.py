4c8ddf025d5279b146e5523b63141bae5b064cca
CSComarch <rajkonkret660@gmail.com>
2023-06-20 13:49:31 +0200

    add arkusz2.py

---
+++ arkusz.py
@@ -0,0 +1,15 @@
+import openpyxl
+
+# pip install openpyxl
+workbook = openpyxl.load_workbook('sales.xlsx')
+worksheet = workbook.active
+
+lista = []
+
+for i in range(0, worksheet.max_row):
+    for col in worksheet.iter_cols(1, worksheet.max_column):
+        lista.append(col[i].value)
+
+print(lista)
+print(lista[0])
+print(lista[0:3])  # ['Sales Date', 'Sales Person', 'Amount']
---
+++ arkusz2.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+excel_data = pd.read_excel('sales.xlsx')
+data = pd.DataFrame(excel_data)
+
+print("The content is:\n", data)
+
+# The content is:
+#    Sales Date    Sales Person  Amount
+# 0 2018-05-12      Sila Ahmed   60000
+# 1 2019-12-06     Mir Hossain   50000
+# 2 2020-08-09    Sarmin Jahan   45000
+# 3 2021-04-07  Mahmudul Hasan   30000
+print(data.columns)  # Index(['Sales Date', 'Sales Person', 'Amount'], dtype='object')
+print(data.values)  # [Timestamp('2020-08-09 00:00:00') 'Sarmin Jahan' 45000]
